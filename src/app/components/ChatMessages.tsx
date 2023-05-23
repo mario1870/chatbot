3a280325c9ac463314edf7567e06ba4614fcc7f2
Mario1870 <marioraach01@gmail.com>
2023-05-23 23:16:29 +0200

    23.05

--- src/app/components/ChatMessages.tsx
+++ src/app/components/ChatMessages.tsx
@@ -1,11 +1,28 @@
 
 
-import { FC, HTMLAttributes } from "react"
+import { MessagesContext } from "@/context/messages"
+import { cn } from "@/lib/utils"
+import { FC, HTMLAttributes, useContext } from "react"
 
 interface ChatMessagesProps extends HTMLAttributes<HTMLDivElement> {}
 
 const ChatMessages: FC<ChatMessagesProps> = ({ className, ...props }) => {
-    return <div {...props}>ChatMessage</div>
+
+    const { messages } = useContext(MessagesContext)
+    const inverseMessages = [...messages].reverse()
+
+
+    return <div {...props} className={cn(
+        "flex flex-col-revers gap-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch", 
+        className
+    )}>
+        <div className="flex-1 flex-grow" />
+        {inverseMessages.map((message) => (
+            <div key={message.id} className="chat-message">
+            </div>
+        ))}
+        
+        ChatMessage</div>
 }
 
 export default ChatMessages
